77d3a595c027253bea027ffe54adce93cec57453
Juan Francisco Bue <buejuanfrancisco@gmail.com>
2024-12-09 16:41:31 -0300

    Creando de auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
 import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TodosModule, CategoriesModule],
+  imports: [UsersModule, TodosModule, CategoriesModule, AuthModule],
   controllers: [],
   providers: [],
 })
